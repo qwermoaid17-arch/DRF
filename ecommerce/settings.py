880bd5598d2437273bdf08de9295d72f03f6346a
maoyed <qwermoaid17@gmail.com>
2026-08-29 13:04:43 +0200

    Day 4 | Ep 8: Implemented Global and View-Specific Pagination in DRF
    Detailed Progress:
    - Configured default PageNumberPagination globally inside settings.py with PAGE_SIZE = 3.
    - Explicitly assigned pagination_class = PageNumberPagination within Products_View_Set.
    - Optimized API response payload by chunking product querysets into paginated JSON structures.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -147,6 +147,11 @@ MEDIA_ROOT = BASE_DIR/'static'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE' : 3
+}
+
 
 
 
