7bd10ffa929ee89ad6df59362c66bc574d3b64aa
maoyed <qwermoaid17@gmail.com>
2026-08-30 10:21:13 +0200

    Day 5 | Ep 9: Added Nested Category Serializer
    Detailed Progress:
    - Used CategorySerial inside ProductSerial to display category details.
    - Changed category output from ID number to full object (title, slug).
    - Improved API response format for easier frontend integration.

--- api/serializers.py
+++ api/serializers.py
@@ -1,16 +1,20 @@
 from rest_framework import serializers
 from storeapp.models import *
 
-class ProductSerial(serializers.ModelSerializer):
-
-    class Meta:
-
-        model = Product
-        fields = ['id', 'name', 'description', 'category', 'slug', 'inventory', 'old_price', 'price']
-
 class CategorySerial(serializers.ModelSerializer):
 
     class Meta:
 
         model = Category
         fields = ['category_id', 'title',  'slug']
+
+
+class ProductSerial(serializers.ModelSerializer):
+
+    category = CategorySerial()
+
+    class Meta:
+
+        model = Product
+        fields = ['id', 'name', 'description', 'category', 'slug', 'inventory', 'old_price', 'price']
+
